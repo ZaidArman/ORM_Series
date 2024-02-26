66729db3f86b22d51ed582a5fa70bcf89b15537e
Zaid <zahidu864@gmai.com>
2024-02-26 18:10:18 +0500

    ORM Series Learning

--- restaurantProject/restaurantProject/urls.py
+++ restaurantProject/restaurantProject/urls.py
@@ -22,5 +22,7 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('core.urls')),
+    
+    # these url for django_debug_toolbar
     path("__debug__/", include("debug_toolbar.urls")),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
