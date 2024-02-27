6dd7ba268f847f7656ed58c468b36e323d3cb552
Zaid <zahidu864@gmai.com>
2024-02-27 22:21:19 +0500

    ORM_Series till Aggretation

--- restaurantProject/core/models.py
+++ restaurantProject/core/models.py
@@ -4,18 +4,21 @@ from django.core.validators import MinValueValidator, MaxValueValidator
 from django.core.exceptions import ValidationError
 from django.db.models.functions import Lower
 
-# We are gointg to create 4 models: 
-    # restuarants
+""" We are gointg to create 4 models: 
+    # Restuarants
     # User :- will use the BuildIn Model
     # rating
     # Sales
+    # Staff :- M2M relationship Model/Table with Restuarants
+    # StaffRestuarant :- A Conjuction Model/Table
+"""
 
-# custo Validators for restuarant name
+""" custo Validators for restuarant name """
 def restuarant_name_begin_with_a(value):
     if not value.startswith('a'):
         raise ValidationError('Restuarant name must be begin with "a" ')
 
-# Restuarant Model to store data about restuarant
+""" Restuarant Model to store data about restuarant"""
 class Restuarants(models.Model):
     class TypeChoices(models.TextChoices):
         PAKISTAN = "PAK", "Pakistan"
@@ -39,7 +42,7 @@ class Restuarants(models.Model):
     restuarant_type = models.CharField(max_length=3, choices=TypeChoices.choices)
     
     class Meta:
-        # here wo do by defualt ordering with lower case name
+        """ here wo do by defualt ordering with lower case name """
         ordering = [Lower('date_opened')]
         get_latest_by = 'date_opened' # by defualt show the last value ordered by date_opened
     
@@ -50,6 +53,7 @@ class Restuarants(models.Model):
         print(self._state.adding)
         super().save(*args, **kwargs)
 
+""" A staff model that have M2M relationship"""
 class Staff(models.Model):
     staff_name = models.CharField(max_length=100)
     restuarant = models.ManyToManyField(Restuarants, through='StaffRestuarant')
@@ -58,7 +62,7 @@ class Staff(models.Model):
         return self.staff_name
     
 """
-if we want to create a custom seperate table between Staff & Restuarant, 
+A model/table (Conjuction), where, if we want to create a custom seperate table between Staff & Restuarant for (M2M relaationship), 
 for that we can use Through method
 """
 class StaffRestuarant(models.Model):
@@ -66,12 +70,12 @@ class StaffRestuarant(models.Model):
     restuarant = models.ForeignKey(Restuarants, on_delete=models.CASCADE)
     salary = models.FloatField(null=True)
 
-# Rating Model to store data about Rating of Costumers
+""" Rating Model to store data about Rating of Costumers """
 class Rating(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     restuarant = models.ForeignKey(Restuarants, on_delete=models.CASCADE, related_name='ratings')
     
-    # Use Min and max Field-Validator
+    """ Use Min and max Field-Validator """
     rating = models.PositiveSmallIntegerField(
         validators = [MinValueValidator(1), MaxValueValidator(5)]
     )
@@ -80,10 +84,9 @@ class Rating(models.Model):
         return f"Rating {self.rating}"
 
 
-# Sales Model to store data about Sales
+""" Sales Model to store data about Sales """
 class Sales(models.Model):
     restuarant = models.ForeignKey(Restuarants, on_delete=models.SET_NULL, null=True, related_name='sales')
     income = models.DecimalField(max_digits=10, decimal_places=2)
     datetime = models.DateTimeField()
-    
     
